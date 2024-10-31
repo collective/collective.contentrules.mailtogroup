e8ee2e448ad4214caf0b28240b2ec6a39780dafd
Espen Moe-Nilssen <espen@medialog.no>
2024-10-31 16:26:06 +0100

    Update mail.py

--- collective/contentrules/mailtogroup/actions/mail.py
+++ collective/contentrules/mailtogroup/actions/mail.py
@@ -196,7 +196,8 @@ class MailActionExecutor:
         msg.attach(MIMEText(message, 'plain', self.email_charset))
         
         if self.element.include_file == True:
-            part = MIMEBase('application', 'octet-stream')
+            import pdb; pdb.set_trace()
+            part = MIMEBase('application', obj.contentType)
             part.set_payload(obj.file.data)  # Assuming blob_file.data provides the binary content
             #encoders.encode_base64(part)
             part.add_header('Content-Disposition', f'attachment; filename={obj.file.filename}')  # Assuming blob_file.filename provides the file name
